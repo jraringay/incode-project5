79939c8b9a09e03b30a19a35e700c1b7c34aac11
Elly H <b.hatooka@gmail.com>
2021-03-15 22:54:11 +1100

    Average also now displays properly for new unrated movies

--- routes/movie.js
+++ routes/movie.js
@@ -29,6 +29,7 @@ router.get("/:movie_id", (req, res) => {
   
   let movie_id = req.params.movie_id
   const user = req.user;
+  let averageRating = 0;
 
   //User-defined functions
 
@@ -40,23 +41,34 @@ router.get("/:movie_id", (req, res) => {
       //console.log(result.rows)
       let total = 0
       let n = result.rows.length
+      if (n > 0) {
       for (i = 0; i < n; i++) {
         total += result.rows[i].rating_score
         //console.log(total)
       }
-      let averageRating = total/n
-
+      averageRating = `Average: ${total/n.toFixed(2)}`
+    }
+    else averageRating = "This movie has yet to be rated"
   res.render("pages/movie", {
     title: "Movie",
     movie_id: movie_id,
     user: user,
-    averageDisplayed: averageRating.toFixed(2)
+    averageDisplayed: averageRating
   });
 })
 .catch((err) => {
   throw err
 })
 })
+/*
+ <h2 class="title"></h2>
+
+        <% if(${averageDisplayed} === 0.00) { %>
+          <h2 class = "user-rating"> This movie has yet to be rated! Be the first! </h2>
+          <% } else { %> 
+          <h2 class = "user-rating"> This is our user average <%= averageDisplayed %> </h2>
+          <% } %>
+          */
 
 /* Export router to app.js */
 module.exports = router;
